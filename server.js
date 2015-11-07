4030a2abc03c7507db30a8e94273e3e6d5ea49e6
Frederik Wouters <woutersf@gmail.com>
2015-11-07 14:49:31 +0100

    add password for admin area

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ var mime = require('mime');
 var qs = require('querystring');
 var amqp = require('amqp');
 var util = require('util');
+var auth = require('http-auth');
+
 var functions = require('./functions');
 var data = require('./data');
+
 var config = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'));
+global.config = config;
+
 if (config.log.logToFile) {
     var log_file = fs.createWriteStream(config.log.logFile, {flags : 'a'});
 }
 var log_stdout = process.stdout;
-
-global.config = config;
-
 
 /**
  * Log to a file and in the terminal.
@@ -30,17 +32,11 @@ console.log = function(d) {
 };
 
 
-// Test unhandled errors
-// setTimeout(function() {
-//     var err = new Error('this is an example error')
-//     throw err
-// }, 5000);
-//Global variable
 
+//////////////////    AMQP   //////////////////////
 //global amqp connection
 var amqpConnection;
 
-//////////////////    AMQP   //////////////////////
 /**
  * connect to amqp server and listen to all queues defined in "listeners"
  */
@@ -112,110 +108,6 @@ if (config.amqp.useamq) {
 
 
 
-//////////////////    WEB   //////////////////////
-var server = http.createServer( function(req, res) {
-    if (functions.requestIsStatic(req,res)) {
-        functions.serveStatic(req,res);
-    }
-    if (req.method == 'POST' && config.web.useweb) {
-        console.log('=============POST================');
-        console.log(req.url);
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            if (body.length > 1e6) {
-                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-                request.connection.destroy();
-            }
-        });
-        req.on('end', function () {
-            if (req.url == '/admin/senders') {
-                var decodedBody = qs.parse(body);
-                functions.writeSettings('./data/senders.inc', decodedBody['senders'], function(err){
-                    if (err) {
-                        console.log('Error writing file');
-                        console.log(err);
-                    }
-                    var html = fs.readFileSync('./html/saved.html');
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(html);
-                });
-            }else if (req.url == '/admin/listeners') {
-                var decodedBody = qs.parse(body);
-                functions.writeSettings('./data/listeners.inc', decodedBody['listeners'], function(err){
-                    if (err) {
-                        console.log('Error writing file');
-                        console.log(err);
-                    }
-                    var html = fs.readFileSync('./html/saved.html');
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(html);
-                });
-            }else{
-                if (global.config.web.requirepostsecret && (typeof req.headers.requestadorsecret == 'undefined' || req.headers.requestadorsecret != global.config.web.requestadorsecret)){
-                    res.writeHead(401);
-                    res.end('Secret not in header or secret incorrect.');
-                }
-                data.getSenders(function(senders){
-                    data.getListeners(function(listeners){
-                        var val = functions.loopListeners(listeners, senders, req, req.method, req.url, body);
-                        if (val){
-                            res.writeHead(200, {'Content-Type': 'text/html'});
-                            res.end('received');
-                        }else{
-                            res.writeHead(404, {'Content-Type': 'text/html'});
-                            res.end('It works!');
-                        }
-                        var html = fs.readFileSync('./html/received.html');
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.end(html);
-                    });
-                });
-            }
-        });
-        // res.writeHead(200, {'Content-Type': 'text/html'});
-        // res.end('post received');
-    }
-    else if(req.method == 'GET' && config.web.useweb)
-    {
-        if (req.url == global.config.web.webpollurl) {
-            var newhtml = 'ok';
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(newhtml);
-        }else if ((req.url == '/admin' || req.url == '/admin/') && global.config.web.webadmin) {
-            var html = fs.readFileSync('./html/admin.html');
-            html = html.toString();
-            var listeners = fs.readFileSync('./data/listeners.inc' );
-            var senders = fs.readFileSync('./data/senders.inc' );
-            var newhtml = html.replace('{listeners}',listeners);
-            newhtml = newhtml.replace('{senders}',senders);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(newhtml );
-        }else{
-            console.log('=============GET================');
-            console.log(req.url);
-            data.getSenders(function(senders){
-                data.getListeners(function(listeners){
-                    var val = functions.loopListeners(listeners, senders, req, req.method, req.url, body);
-                    if (val){
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.end('received');
-                    }else{
-                        res.writeHead(404, {'Content-Type': 'text/html'});
-                        res.end('nothing here');
-                    }
-                    var html = fs.readFileSync('./html/received.html');
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(html);
-
-                });
-            });
-        }
-    }else{
-        console.log('[WEB] unsupported request type or server not enabled in config');
-    }
-});
-global.server = server;
 
 
 
@@ -225,7 +117,6 @@ if (config.server.usesocketio) {
     global.io = require('socket.io')(server);
     console.log('[IO] attempt connect');
     global.io.on('connection', function(socket){
-
         global.socket = socket;
         console.log('[IO] connect');
         data.getSenders(function(senders){
@@ -261,14 +152,186 @@ if (config.server.usesocketio) {
 
 
 
+
+//////////////////    WEB   //////////////////////
+var server = http.createServer( function(req, res) {
+    if (functions.requestIsStatic(req,res)) {
+        functions.serveStatic(req,res);
+    }
+    if (req.method == 'POST' && config.web.useweb) {
+        console.log('=============POST================');
+        console.log(req.url);
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            if (body.length > 1e6) {
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                request.connection.destroy();
+            }
+        });
+        req.on('end', function () {
+            if (global.config.web.requirepostsecret && (typeof req.headers.requestadorsecret == 'undefined' || req.headers.requestadorsecret != global.config.web.requestadorsecret)){
+                res.writeHead(401);
+                res.end('Secret not in header or secret incorrect.');
+            }
+            data.getSenders(function(senders){
+                data.getListeners(function(listeners){
+                    var val = functions.loopListeners(listeners, senders, req, req.method, req.url, body);
+                    if (val){
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.end('received');
+                    }else{
+                        res.writeHead(404, {'Content-Type': 'text/html'});
+                        res.end('It works!');
+                    }
+                    var html = fs.readFileSync('./html/received.html');
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(html);
+                });
+            });
+        });
+        // res.writeHead(200, {'Content-Type': 'text/html'});
+        // res.end('post received');
+    }
+    else if(req.method == 'GET' && config.web.useweb)
+    {
+
+        if (req.url == global.config.web.webpollurl) {
+            var newhtml = 'ok';
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(newhtml);
+        }else{
+            console.log('=============GET================');
+            console.log(req.url);
+            data.getSenders(function(senders){
+                data.getListeners(function(listeners){
+                    var val = functions.loopListeners(listeners, senders, req, req.method, req.url, body);
+                    if (val){
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.end('200:ok');
+                    }else{
+                        res.writeHead(404, {'Content-Type': 'text/html'});
+                        res.end('404:Page not found');
+                    }
+                    var html = fs.readFileSync('./html/received.html');
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(html);
+
+                });
+            });
+        }
+    }else{
+        console.log('[WEB] unsupported request type or server not enabled in config');
+    }
+});
+global.server = server;
+
+
+
+
+
+
+
+//////////////////    ADMINWEB   //////////////////////
+
+
+var basic = auth.basic({
+    realm: "Requestador",
+    file: __dirname + "/users.htpasswd" // gevorg:gpass, Sarah:testpass ...
+});
+
+var adminServer = http.createServer(basic, function(req, res) {
+    console.log('===================');
+    console.log(req.url);
+    console.log('===================');
+    if (functions.requestIsStatic(req,res)) {
+        functions.serveStatic(req,res);
+    }
+    if (req.method == 'POST') {
+        console.log('=============POST================');
+        console.log(req.url);
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            if (body.length > 1e6) {
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                request.connection.destroy();
+            }
+        });
+        req.on('end', function () {
+            if (req.url == '/senders') {
+                var decodedBody = qs.parse(body);
+                functions.writeSettings('./data/senders.inc', decodedBody['senders'], function(err){
+                    if (err) {
+                        console.log('Error writing file');
+                        console.log(err);
+                    }
+                    var html = fs.readFileSync('./html/saved.html');
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(html);
+                });
+            }else if (req.url == '/listeners') {
+                var decodedBody = qs.parse(body);
+                functions.writeSettings('./data/listeners.inc', decodedBody['listeners'], function(err){
+                    if (err) {
+                        console.log('Error writing file');
+                        console.log(err);
+                    }
+                    var html = fs.readFileSync('./html/saved.html');
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(html);
+                });
+            }else{
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('Oops');
+            }
+        });
+    } else
+    {
+        if (req.url == '/') {
+            var html = fs.readFileSync('./html/admin.html');
+            html = html.toString();
+            html = html.replace('{{socketserver}}','http://' + global.config.server.ip + ':' + global.config.server.port);
+
+            var listeners = fs.readFileSync('./data/listeners.inc' );
+            var senders = fs.readFileSync('./data/senders.inc' );
+            var newhtml = html.replace('{listeners}',listeners);
+            newhtml = newhtml.replace('{senders}',senders);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(newhtml );
+        }else
+        {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end('Oops');
+        }
+    }
+});
+
+
+
+
 //////////////////    START   //////////////////////
+
 port = config.server.port;
 host = config.server.ip;
 if (config.server.useweb || config.server.usesocketio) {
     server.listen(port, host);
+    console.log('[WEB] Listening at http://' + host + ':' + port);
 }else{
     console.log('[WEB] server disabled in config');
 }
 
-console.log('Listening at http://' + host + ':' + port);
+
+//////////////////   ADMINSERVER START   //////////////////////
+
+adminport = config.adminserver.port;
+adminhost = config.adminserver.ip;
+if (config.adminserver.enabled) {
+    adminServer.listen(adminport, adminhost);
+    console.log('[ADMINWEB]Listening at http://' + adminhost + ':' + adminport);
+}else{
+    console.log('[ADMINWEB] server disabled in config');
+}
+
+
 
